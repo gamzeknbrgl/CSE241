c8b729808e5c76c0587e54081fada636597baae0
gamzeknbrgl <43278655+gamzeknbrgl@users.noreply.github.com>
2019-01-21 00:09:32 +0300

    -

---
+++ HW1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#define PI 3.14
+#include "source.h"
+enum class shapes {rectangle, circle, triangle};
+using namespace std;
+int main(){
+	choose_shapes();
+	return 0;
+}
